5ce98316123d8154bab697923f14862c5470f9d8
Ashley Davis <ashley@codecapers.com.au>
2023-03-12 15:48:39 +1000

    Changed the layout algorithm so that each row of items is stretched slightly beyond the boundary.
    This change helps keep items better aligned to the right hand boundary.
    I've exposed the width of each row here primarily for testing, the gallery component doesn't use this for rendering.
    Also saving the computed width/height for each item the gallery.

--- src/test/layout.test.js
+++ src/test/layout.test.js
@@ -9,7 +9,7 @@ describe("layout", () => {
         expect(rows).toEqual([]);
     });
 
-    test("can layout single item", () => {
+    test("can layout single item that is smaller than the gallery", () => {
 
         const targetRowHeight = 115;
         const item = {
@@ -17,17 +17,44 @@ describe("layout", () => {
             height: 100,
         };
 
-        const rows = createLayout([ item ], targetRowHeight);
+		const galleryWidth = 1200;
+        const rows = createLayout([ item ], targetRowHeight, galleryWidth);
 
         expect(rows.length).toBe(1);
         
         const row = rows[0];
         expect(row.height).toBe(targetRowHeight);
+		expect(row.width).toBeLessThan(galleryWidth);
         expect(row.items.length).toBe(1);
 
         const layoutItem = row.items[0];
-        expect(layoutItem.width).toBe(item.width);
-        expect(layoutItem.height).toBe(item.height);
+        expect(layoutItem.width).toBeGreaterThan(item.width);
+        expect(layoutItem.height).toBe(targetRowHeight);
+    });
+    
+   test("can layout single item that is larger than the gallery", () => {
+
+        const targetRowHeight = 118;
+        const items = [
+            {
+                width: 1200,
+                height: 500,
+            },
+        ];
+
+        const galleryWidth = 600;
+        const rows = createLayout(items, targetRowHeight, galleryWidth);
+
+        expect(rows.length).toBe(1);
+        
+        const row = rows[0];
+        expect(row.height).toBeGreaterThanOrEqual(targetRowHeight);
+        expect(row.width).toBeLessThan(galleryWidth);
+        expect(row.items.length).toBe(1);
+
+        const layoutItem = row.items[0];
+        expect(layoutItem.width).toBeLessThan(items[0].width);
+        expect(layoutItem.height).toBeLessThan(items[0].height);
     });
 
     test("can layout multiple items", () => {
@@ -46,59 +73,18 @@ describe("layout", () => {
             height: 50,
         };
 
-        const rows = createLayout([ item1, item2, item3 ], targetRowHeight);
+		const galleryWidth = 1200;
+        const rows = createLayout([ item1, item2, item3 ], targetRowHeight, galleryWidth);
 
         expect(rows.length).toBe(1);
         
         const row = rows[0];
         expect(row.height).toBe(targetRowHeight);
+        expect(row.width).toBeLessThan(galleryWidth);
         expect(row.items.length).toBe(3);
-        expect(row.items[0].width).toBe(item1.width);
-        expect(row.items[0].height).toBe(item1.height);
-        expect(row.items[1].width).toBe(item2.width);
-        expect(row.items[1].height).toBe(item2.height);
-        expect(row.items[2].width).toBe(item3.width);
-        expect(row.items[2].height).toBe(item3.height);
     });
     
     test("items wrap to next line on overflow", () => {
-
-        const targetRowHeight = 110;
-        const items = [ 
-            {
-                width: 200,
-                height: 100,
-            },
-            {
-                width: 300,
-                height: 140,
-            },
-            {
-                width: 180,
-                height: 30,
-            },
-        ];
-        
-        const rows = createLayout(items, targetRowHeight, 600);
-
-        expect(rows.length).toBe(2);
-        
-        const firstRow = rows[0];
-        expect(firstRow.height).toBeGreaterThan(targetRowHeight);
-        expect(firstRow.items.length).toBe(2);
-        expect(firstRow.items[0].width).toBeGreaterThan(items[0].width);
-        expect(firstRow.items[0].height).toBeGreaterThan(items[0].height);
-        expect(firstRow.items[1].width).toBeGreaterThan(items[1].width);
-        expect(firstRow.items[1].height).toBeGreaterThan(items[1].height);
-        
-        const secondRow = rows[1];
-        expect(secondRow.height).toBe(targetRowHeight);
-        expect(secondRow.items.length).toBe(1);
-        expect(secondRow.items[0].width).toBe(items[2].width);
-        expect(secondRow.items[0].height).toBe(items[2].height);
-    });    
-    
-    test("items that are not on the last row should stretch toward the boundary", () => {
 
         const targetRowHeight = 110;
         const items = [ 
@@ -120,6 +106,44 @@ describe("layout", () => {
 
         expect(rows.length).toBe(2);
         
+        const firstRow = rows[0];
+        expect(firstRow.height).toBeGreaterThan(targetRowHeight);
+        expect(firstRow.items.length).toBe(2);
+        expect(firstRow.items[0].width).toBeGreaterThan(items[0].width);
+        expect(firstRow.items[0].height).toBeGreaterThan(items[0].height);
+        expect(firstRow.items[1].width).toBeGreaterThan(items[1].width);
+        expect(firstRow.items[1].height).toBeGreaterThan(items[1].height);
+        
+        const secondRow = rows[1];
+        expect(secondRow.height).toBe(targetRowHeight);
+        expect(secondRow.items.length).toBe(1);
+        expect(secondRow.items[0].width).toBeGreaterThan(items[2].width);
+        expect(secondRow.items[0].height).toBe(targetRowHeight);
+    });    
+    
+    test("items that are not on the last row should stretch toward the boundary", () => {
+
+        const targetRowHeight = 110;
+        const items = [ 
+            {
+                width: 200,
+                height: 100,
+            },
+            {
+                width: 300,
+                height: 140,
+            }, 
+            {
+                width: 180,
+                height: 30,
+            },
+        ];
+        
+        const galleryWidth = 670;
+        const rows = createLayout(items, targetRowHeight, galleryWidth);
+
+        expect(rows.length).toBe(2);
+        
         //
         // The first row is stretched out toward the boundary.
         //
@@ -137,17 +161,25 @@ describe("layout", () => {
         expect(firstRow.height).toBeGreaterThan(targetRowHeight);
 
         //
+        // The first frow must stretch to the width of the gallery.
+        //
+        expect(firstRow.width).toBeGreaterThanOrEqual(galleryWidth);
+
+        //
         // The second row is not stretched.
         //        
         const secondRow = rows[1];
         expect(secondRow.items.length).toBe(1);
-        expect(secondRow.items[0].width).toBe(items[2].width);
-        expect(secondRow.items[0].height).toBe(items[2].height);
 
         // 
         // The height of the second row is the target.
         //
         expect(secondRow.height).toBe(targetRowHeight);
+
+        //
+        // The second row doesn't extend to the width of the gallery.
+        //
+        expect(secondRow.width).toBeLessThan(galleryWidth);
 
     });        
 });
