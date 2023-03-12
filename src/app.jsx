44421b2a3371315dda3a324dd76fa3e5e9ea6946
Ashley Davis <ashley@codecapers.com.au>
2023-03-12 15:49:08 +1000

    Now displaying all items in the gallery to visually test the gallery layout logic so far.

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-// const gallery = require("./gallery.json");
+const gallery = require("./gallery.json");
 const { Gallery } = require("./gallery");
 
 //
@@ -34,23 +34,23 @@ const { Gallery } = require("./gallery");
 //
 // NOTE: I've adjusted these values by hand to overflow the boundary.
 //
-const gallery = [
-    {
-        thumb: "https://via.placeholder.com/200x100",
-        width: 200,
-        height: 100,
-    },
-    {
-        thumb: "https://via.placeholder.com/300x140",
-        width: 300,
-        height: 140,
-    },
-    {
-        thumb: "https://via.placeholder.com/180x80",
-        width: 180,
-        height: 30,
-    },
-];
+// const gallery = [ 
+//     {
+//         thumb: "https://via.placeholder.com/200x100",
+//         width: 200,
+//         height: 100,
+//     },
+//     {
+//         thumb: "https://via.placeholder.com/300x140",
+//         width: 300,
+//         height: 140,
+//     },
+//     {
+//         thumb: "https://via.placeholder.com/180x80",
+//         width: 180,
+//         height: 30,
+//     },
+// ];
 
 export function App() {
     return (
